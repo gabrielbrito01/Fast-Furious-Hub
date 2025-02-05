a7951d1dd99f8ea9ad4a074ab8e797d9853ca639
gabrielbrito01 <gabrielbr.albuquerque@gmail.com>
2025-02-05 10:04:51 -0300

    otimizando imagens

--- fast-furious-hub/app/components/carousel/Carousel.tsx
+++ fast-furious-hub/app/components/carousel/Carousel.tsx
@@ -1,63 +1,97 @@
+import Image from "next/image";
 import {
-    Carousel,
-    CarouselContent,
-    CarouselItem,
-    CarouselNext,
-    CarouselPrevious,
-  } from "../ui/carousel";
-  
-  export default function CarouselFilmes() {
-    return (
-      <Carousel className="w-full max-w-2xl mx-auto">
-        <CarouselContent className="flex gap-4 p-2.5">
-          <CarouselItem className="flex flex-col justify-center items-center text-center">
-            <img className="w-64 h-96 rounded-lg shadow-lg" src="/velozes1.png" alt="Velozes e Furiosos 1 (2001)" />
-            <p className="mt-2 text-lg font-semibold">Velozes e Furiosos 1 (2001)</p>
-          </CarouselItem>
-          <CarouselItem className="flex flex-col justify-center items-center text-center">
-            <img className="w-64 h-96 rounded-lg shadow-lg" src="/velozes2.png" alt="+Velozes +Furiosos (2003)" />
-            <p className="mt-2 text-lg font-semibold">+Velozes +Furiosos (2003)</p>
-          </CarouselItem>
-          <CarouselItem className="flex flex-col justify-center items-center text-center">
-            <img className="w-64 h-96 rounded-lg shadow-lg" src="/velozes4.png" alt="Velozes e Furiosos 4 (2009)" />
-            <p className="mt-2 text-lg font-semibold">Velozes e Furiosos 4 (2009)</p>
-          </CarouselItem>
-          <CarouselItem className="flex flex-col justify-center items-center text-center">
-            <img className="w-64 h-96 rounded-lg shadow-lg" src="/velozes5.png" alt="Velozes e Furiosos 5: Operação Rio (2011)" />
-            <p className="mt-2 text-lg font-semibold">Velozes e Furiosos 5: Operação Rio (2011)</p>
-          </CarouselItem>
-          <CarouselItem className="flex flex-col justify-center items-center text-center">
-            <img className="w-64 h-96 rounded-lg shadow-lg" src="/velozes6.png" alt="Velozes e Furiosos 6 (2013)" />
-            <p className="mt-2 text-lg font-semibold">Velozes e Furiosos 6 (2013)</p>
-          </CarouselItem>
-          <CarouselItem className="flex flex-col justify-center items-center text-center">
-            <img className="w-64 h-96 rounded-lg shadow-lg" src="/velozes3.png" alt="Velozes e Furiosos: Desafio em Tóquio (2006)" />
-            <p className="mt-2 text-lg font-semibold">Velozes e Furiosos: Desafio em Tóquio (2006)</p>
-          </CarouselItem>
-          <CarouselItem className="flex flex-col justify-center items-center text-center">
-            <img className="w-64 h-96 rounded-lg shadow-lg" src="/velozes7.png" alt="Velozes e Furiosos 7 (2015)" />
-            <p className="mt-2 text-lg font-semibold">Velozes e Furiosos 7 (2015)</p>
-          </CarouselItem>
-          <CarouselItem className="flex flex-col justify-center items-center text-center">
-            <img className="w-64 h-96 rounded-lg shadow-lg" src="/velozes8.png" alt="Velozes e Furiosos 8 (2017)" />
-            <p className="mt-2 text-lg font-semibold">Velozes e Furiosos 8 (2017)</p>
-          </CarouselItem>
-          <CarouselItem className="flex flex-col justify-center items-center text-center">
-            <img className="w-64 h-96 rounded-lg shadow-lg" src="/hobbs.png" alt="Velozes e Furiosos: Hobbs & Shaw (2019)" />
-            <p className="mt-2 text-lg font-semibold">Velozes e Furiosos: Hobbs & Shaw (2019)</p>
-          </CarouselItem>
-          <CarouselItem className="flex flex-col justify-center items-center text-center">
-            <img className="w-64 h-96 rounded-lg shadow-lg" src="/velozes9.png" alt="Velozes e Furiosos 9 (2021)" />
-            <p className="mt-2 text-lg font-semibold">Velozes e Furiosos 9 (2021)</p>
-          </CarouselItem>
-          <CarouselItem className="flex flex-col justify-center items-center text-center">
-            <img className="w-64 h-96 rounded-lg shadow-lg" src="/velozes10.png" alt="Velozes e Furiosos 10 (2023)" />
-            <p className="mt-2 text-lg font-semibold">Velozes e Furiosos 10 (2023)</p>
-          </CarouselItem>
-        </CarouselContent>
-        <CarouselPrevious className="absolute left-0" />
-        <CarouselNext className="absolute right-0" />
-      </Carousel>
-    );
-  }
-  
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "../ui/carousel";
+import Imagens from "../imagens/ImagemOtimizada";
+import velozes1 from "@/assets/velozes1.png";
+import velozes2 from "@/assets/velozes2.png";
+import velozes3 from "@/assets/velozes3.png";
+import velozes4 from "@/assets/velozes4.png";
+import velozes5 from "@/assets/velozes5.png";
+import velozes6 from "@/assets/velozes6.png";
+import velozes7 from "@/assets/velozes7.png";
+import velozes8 from "@/assets/velozes8.png";
+import hobbs from "@/assets/hobbs.png";
+import velozes9 from "@/assets/velozes9.png";
+import velozes10 from "@/assets/velozes10.png";
+
+export default function CarouselFilmes() {
+  return (
+    <Carousel className="w-full max-w-2xl mx-auto">
+      <CarouselContent className="flex gap-4 p-2.5">
+        <CarouselItem className="flex flex-col justify-center items-center text-center">
+          <Imagens alt="velozes1" src={velozes1} />
+          <p className="mt-2 text-lg font-semibold">
+            Velozes e Furiosos 1 (2001)
+          </p>
+        </CarouselItem>
+        <CarouselItem className="flex flex-col justify-center items-center text-center">
+          <Imagens alt="velozes2" src={velozes2} />
+          <p className="mt-2 text-lg font-semibold">
+            +Velozes +Furiosos (2003)
+          </p>
+        </CarouselItem>
+        <CarouselItem className="flex flex-col justify-center items-center text-center">
+          <Imagens alt="velozes4" src={velozes4} />
+          <p className="mt-2 text-lg font-semibold">
+            Velozes e Furiosos 4 (2009)
+          </p>
+        </CarouselItem>
+        <CarouselItem className="flex flex-col justify-center items-center text-center">
+          <Imagens alt="velozes5" src={velozes5} />
+          <p className="mt-2 text-lg font-semibold">
+            Velozes e Furiosos 5: Operação Rio (2011)
+          </p>
+        </CarouselItem>
+        <CarouselItem className="flex flex-col justify-center items-center text-center">
+          <Imagens alt="velozes6" src={velozes6} />
+          <p className="mt-2 text-lg font-semibold">
+            Velozes e Furiosos 6 (2013)
+          </p>
+        </CarouselItem>
+        <CarouselItem className="flex flex-col justify-center items-center text-center">
+          <Imagens alt="velozes3" src={velozes3} />
+          <p className="mt-2 text-lg font-semibold">
+            Velozes e Furiosos: Desafio em Tóquio (2006)
+          </p>
+        </CarouselItem>
+        <CarouselItem className="flex flex-col justify-center items-center text-center">
+          <Imagens alt="velozes7" src={velozes7} />
+          <p className="mt-2 text-lg font-semibold">
+            Velozes e Furiosos 7 (2015)
+          </p>
+        </CarouselItem>
+        <CarouselItem className="flex flex-col justify-center items-center text-center">
+          <Imagens alt="velozes8" src={velozes8} />
+          <p className="mt-2 text-lg font-semibold">
+            Velozes e Furiosos 8 (2017)
+          </p>
+        </CarouselItem>
+        <CarouselItem className="flex flex-col justify-center items-center text-center">
+          <Imagens alt="hobbs" src={hobbs} />
+          <p className="mt-2 text-lg font-semibold">
+            Velozes e Furiosos: Hobbs & Shaw (2019)
+          </p>
+        </CarouselItem>
+        <CarouselItem className="flex flex-col justify-center items-center text-center">
+          <Imagens alt="velozes9" src={velozes9} />
+          <p className="mt-2 text-lg font-semibold">
+            Velozes e Furiosos 9 (2021)
+          </p>
+        </CarouselItem>
+        <CarouselItem className="flex flex-col justify-center items-center text-center">
+          <Imagens alt="velozes10" src={velozes10} />
+          <p className="mt-2 text-lg font-semibold">
+            Velozes e Furiosos 10 (2023)
+          </p>
+        </CarouselItem>
+      </CarouselContent>
+      <CarouselPrevious className="absolute left-0" />
+      <CarouselNext className="absolute right-0" />
+    </Carousel>
+  );
+}
--- fast-furious-hub/app/components/imagens/ImagemOtimizada.tsx
+++ fast-furious-hub/app/components/imagens/ImagemOtimizada.tsx
@@ -1,38 +1,23 @@
-import Image from "next/image";
-import sw2 from "@/assets/sw2.png";
+import Image, { StaticImageData } from "next/image";
 
 interface ImageProps {
   alt: string;
   caption?: string;
+  src : string | StaticImageData;
 }
 
-interface ImagensProps {
-  images: ImageProps[];
-}
-
-export default function Imagens({ images }: ImagensProps) {
+export default function Imagens({...ImageProps}: ImageProps) {
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
-      {images.map((image, index) => (
-        <div key={index} className="relative w-full h-64">
+
           <Image
-              src={sw2}
-              alt="Sw"
-              width={500}
-              height={500}
-              layout="responsive"
+              src={ImageProps.src}
+              alt={ImageProps.alt}
+              width={250}
+              height={250}
               objectFit="cover"
-              quality={75}
+              quality={100}
               className="object-center"
               aria-describedby="caption"
             />
-          {image.caption && (
-            <p className="text-center mt-2 text-sm text-gray-600">
-              {image.caption}
-            </p>
-          )}
-        </div>
-      ))}
-    </div>
   );
 }
